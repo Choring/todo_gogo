facefdacba365b2717f301fcbdf9ca83b72db6c8
skyhonor201 <skyhonor201@gmail.com>
2022-04-25 13:42:54 +0900

    ttt

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express') // node_modules 내 express 관련 코드를 가져온다
 var app = express()
 
+// 사용자가 localhost:5000//hello URL 주소로 접근했을때
 app.get('/hello', (req, res) => { // URL 응답 테스트
-    res.send('hello world !')
+    res.send('hello world !') // 응답을 브라우저로 보내줌
 })
 //맨 밑에 놓기
 app.listen(5000, () => { // 5000 포트로 서버 오픈
